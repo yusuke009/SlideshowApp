7f88881a26a21d4df513c3544a56268205cc9d52
yusuke.saito <yusuke009ag@gmail.com>
2020-11-23 12:50:10 +0900

    ①画像をナビゲーションエリアへコピー　②Auto LyoutでUI部品配置　③起動時に画像表示

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
+        imageView.image = imageSlide[index]
         
     }
     
